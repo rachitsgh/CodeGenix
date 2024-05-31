e86208964c0bb576bafc51853eb6bdca37f06d1f
rachitsgh <rachit2351@gmail.com>
2024-05-31 19:40:58 +0530

    Schema of section added

---
+++ SERVER/models/Section.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const sectionSchema=new mongoose.Schema({
+    sectionName:{
+        type:String,
+    },
+    subSection:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"SubSection"
+    }
+
+})
